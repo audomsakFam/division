0b009ea2dc3ee96000c55255640f5faef8490e21
unknown <famqqblood@gmail.com>
2024-11-30 13:09:01 +0700

    push main style

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import HomePage from "./pages/user/home/page";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]
+    flex flex-col">
       <Head>
         <meta name="viewport" content="width=1024" />
         <link rel="icon" href="/favicon.ico" />
--- app/pages/user/home/page.tsx
+++ app/pages/user/home/page.tsx
@@ -1,17 +1,66 @@
 'use client'
+import { ResItemsGroup } from "@/app/interfaces/item";
 import { Button } from "@/components/ui/button";
+import { GetItemWithCache } from "@/lib/servers/getItemWithCache";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
 
 export default function HomePage() {
-const router = useRouter();
+    const router = useRouter();
+    const [items, setItems] = useState<ResItemsGroup[]>([]);
+
+    useEffect(() => {
+        GetItemWithCache().then((res) => setItems(res));
+    }, [])
     return (
-        <>
-            <div>
-                Enter
-            </div>
-            <Button onClick={() => router.push('/pages/admin/login')}>
-                Login
-            </Button>
-        </>
+        <div  className="h-screen overflow-auto"> 
+            <Card className="w-full m-2">
+                <CardHeader>
+                    <CardTitle>Create project</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <Table className="overflow-hidden">
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[100px]">name</TableHead>
+                                <TableHead>img</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {items.map((v,i) => (
+                                <TableRow key={i}>
+                                    <TableCell className="font-medium">{v.name}</TableCell>
+                                    <TableCell>
+                                        {
+                                            v.img ? <img src={v.img} alt="img" className="w-[100px]"/> : 'no img'
+                                        }
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                        <TableFooter>
+                        </TableFooter>
+                    </Table>
+                </CardContent>
+            </Card>
+        </div>
     );
 }
