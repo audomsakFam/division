e5d1f4d43087ce3db19549c42dfe239b460322d6
unknown <your84120@gmail.com>
2025-03-07 12:04:49 +0700

    add requirement

--- app/components/calendar/calendar.tsx
+++ app/components/calendar/calendar.tsx
@@ -23,7 +23,8 @@ export default function CalendarCom() {
                         end: new Date(item.retureAt),    // เปลี่ยนเป็น Date
                     })),
                     returned: res.filter((item) => item.status !== 4).map((item) => new Date(item.retureAt)),
-                    eventDetail: res.filter((item) => item.status !== 4).map((item) => `โครงการ ${item.project} โดย ${item.name}`),
+                    // eventDetail: res.filter((item) => item.status !== 4).map((item) => `โครงการ ${item.project} โดย ${item.name}`),
+                    eventDetail: res.filter((item) => item.status !== 4).map((item) => `${new Date(item.createAt).toLocaleTimeString('th-TH', { hour: '2-digit', minute: '2-digit', hour12: false })} ${item.name}`),
                 });
             });
             setLoading(false);
@@ -32,25 +33,26 @@ export default function CalendarCom() {
     }, []);
 
     // ฟังก์ชันกำหนดสีตามเงื่อนไข
-    const getDayColor = (date: Date) => {
-        const isBorrowed = eventDates.borrowed.some(
-            (event) => date >= event.start && date <= event.end  // เปรียบเทียบ Date โดยตรง
-        );
-        const isReturned = eventDates.returned.some(
-            (d) => date.toISOString().split("T")[0] === d.toISOString().split("T")[0]  // เปรียบเทียบ Date
-        );
-        const today = new Date();
-        const isToday = date.toISOString().split("T")[0] === today.toISOString().split("T")[0];
+    // const getDayColor = (date: Date) => {
+    //     const isBorrowed = eventDates.borrowed.some(
+    //         (event) => date >= event.start && date <= event.end  // เปรียบเทียบ Date โดยตรง
+    //     );
+    //     const isReturned = eventDates.returned.some(
+    //         (d) => date.toISOString().split("T")[0] === d.toISOString().split("T")[0]  // เปรียบเทียบ Date
+    //     );
+    //     const today = new Date();
+    //     const isToday = date.toISOString().split("T")[0] === today.toISOString().split("T")[0];
 
-        if ((isToday && isBorrowed && isReturned) || (isToday && isBorrowed) || (isToday && isReturned)) return "bg-gradient-to-r from-red-500 via-blue-900 to-yellow-500 text-white"; // วันปัจจุบัน
-        if (isBorrowed && isReturned) {
-            return "bg-gradient-to-r from-red-500 to-yellow-500 text-white"; // สีแดงและเหลืองสำหรับยืมและคืนในวันเดียวกัน
-        }
-        if (isBorrowed) return "bg-red-500 text-white"; // วันที่ยืม
-        if (isReturned) return "bg-yellow-500 text-black"; // วันที่คืน
-        if (isToday) return "bg-blue-300 text-black"; // วันที่คืน
-        return "bg-white text-black"; // วันอื่นๆ
-    };
+    //     if ((isToday && isBorrowed && isReturned) || (isToday && isBorrowed) || (isToday && isReturned)) return "bg-gradient-to-r from-red-500 via-blue-900 to-yellow-500 text-white"; // วันปัจจุบัน
+    //     if (isBorrowed && isReturned) {
+    //         return "bg-gradient-to-r from-red-500 to-yellow-500 text-white"; // สีแดงและเหลืองสำหรับยืมและคืนในวันเดียวกัน
+    //     }
+    //     if (isBorrowed) return "bg-red-500 text-white"; // วันที่ยืม
+    //     if (isReturned) return "bg-yellow-500 text-black"; // วันที่คืน
+    //     if (isToday) return "bg-blue-300 text-black"; // วันที่คืน
+    //     return "bg-white text-black"; // วันอื่นๆ
+    // };
+
 
     // ฟังก์ชันแสดงรายละเอียดเหตุการณ์
 
@@ -74,7 +76,7 @@ export default function CalendarCom() {
                     Day: ({ date }) => {
                         const newDate = new Date(date);
                         newDate.setDate(date.getDate() + 1);  // เพิ่ม 1 วัน
-                        const dayClasses = getDayColor(newDate);
+                        // const dayClasses = getDayColor(newDate);
 
                         const eventDetail = eventDates.borrowed
                             .filter((event) => {
@@ -100,16 +102,34 @@ export default function CalendarCom() {
                                 console.log("date event new------->", normalizeDate(newDate).toDateString());
                                 console.log("date event isEndDate------->", isEndDate);
 
-                                const additionalDetail = isEndDate ? "คืน" : "ยืม"; // ถ้าตรงเพิ่ม "คืน" ถ้าไม่ตรงเพิ่ม "ยืม"
+                                // const additionalDetail = isEndDate ? "คืน" : "ยืม"; // ถ้าตรงเพิ่ม "คืน" ถ้าไม่ตรงเพิ่ม "ยืม"
 
-                                return `${baseDetail} (${additionalDetail})`; // เพิ่มข้อความต่อท้าย
+                                // return `${baseDetail} (${additionalDetail})`; // เพิ่มข้อความต่อท้าย
+                                return `${baseDetail} `; // เพิ่มข้อความต่อท้าย
                             });
+                        const getDayDetail = (date: Date) => {
+                            console.log('detail ---> ', eventDetail)
+                            return eventDetail.length > 0 ? eventDetail.join(", ") : '';
+                        };
 
                         const detailToShow = eventDetail.length > 0 ? eventDetail.join('\n') : "ไม่มีข้อมูลกิจกรรม";
                         return (
-                            <div className={`w-full h-full border-y flex items-center justify-center cursor-pointer whitespace-per-line p-0`} title={detailToShow}>
-                                <div className={`${dayClasses} w-full h-[30px] sm:h-[100px] flex items-center justify-center border-r border-gray-500`}>
-                                    {date.getDate()}
+                            // <div className={`w-full h-full border-y flex items-center justify-center cursor-pointer whitespace-per-line p-0`} title={detailToShow}>
+                            //     <div className={`${dayClasses} w-full h-[30px] sm:h-[100px] flex items-center justify-center border-r border-gray-500`}>
+                            //         {date.getDate()}
+                            //     </div>
+                            // </div>
+                            <div
+                                className={`w-full h-full border-y flex items-center justify-center cursor-pointer whitespace-pre-line p-0`}
+                                // className={`w-full h-full border-y flex items-center justify-center cursor-pointer whitespace-pre-line p-0`}
+                                // title={detailToShow}
+                            >
+                                <div className="w-full h-[30px] sm:h-[100px] flex flex-col items-center justify-center border-r border-gray-500">
+                                    <span className={date.toDateString() === new Date().toDateString() ? "text-blue-500 font-bold" : ""}>
+                                        {date.getDate()}
+                                    </span>
+                                      {/* <span>{date.getDate()}</span>  */}
+                                    <span>{getDayDetail(newDate)}</span>
                                 </div>
                             </div>
                         );
@@ -123,7 +143,7 @@ export default function CalendarCom() {
                 defaultMonth={fieldValue?.from}
                 locale={th}
             />
-            <div className="flex flex-col gap-2 ml-12 mt-2 mb-2">
+            {/* <div className="flex flex-col gap-2 ml-12 mt-2 mb-2">
                 <p className="flex items-center">
                     <div className="bg-blue-300 rounded-full w-4 h-4 mr-2"></div>
                     วันนี้
@@ -140,7 +160,7 @@ export default function CalendarCom() {
                     <div className="bg-gradient-to-r from-red-500 via-blue-900 to-yellow-500 rounded-full w-4 h-4 mr-2"></div>
                     วันนี้และมีหลายเหตุการณ์
                 </p>
-            </div>
+            </div> */}
 
         </div>
     );
